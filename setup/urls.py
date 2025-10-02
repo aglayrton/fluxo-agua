9fed0c5190c91c6b3ce7db4684db902152cfd03f
Kauan Rodrigues <kauanrl09@gmail.com>
2025-10-02 10:55:53 -0300

    update

--- setup/urls.py
+++ setup/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('', include('fluxo.urls')),
 
     # Swagger/OpenAPI documentation
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^docs/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
